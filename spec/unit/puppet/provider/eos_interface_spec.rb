7f76c9e96c51a2c216b2068f7b062ca1de7f6050
Peter Sprygada <sprygada@arista.com>
2015-02-08 21:39:12 -0500

    adds unit test cases for eos_interface
    This commit adds unit test cases for the eos_interface module.  All test
    cases are passing.  This commit makes no additional changes and is safe for
    general application

--- spec/unit/puppet/provider/eos_interface_spec.rb
+++ spec/unit/puppet/provider/eos_interface_spec.rb
@@ -39,8 +39,6 @@ describe Puppet::Type.type(:eos_interface).provider(:eos) do
       name: 'Ethernet1',
       description: 'test interface',
       enable: :true,
-      flowcontrol_send: :on,
-      flowcontrol_receive: :off,
       provider: described_class.name
     }
     Puppet::Type.type(:eos_interface).new(resource_hash)
@@ -48,38 +46,38 @@ describe Puppet::Type.type(:eos_interface).provider(:eos) do
 
   let(:provider) { resource.provider }
 
-  def all_interfaces
-    all_interfaces = Fixtures[:all_interfaces]
-    return all_interfaces if all_interfaces
-    file = File.join(File.dirname(__FILE__), 'fixtures/interfaces.json')
-    Fixtures[:all_interfaces] = JSON.load(File.read(file))
+  let(:api) { double('rbeapi').as_null_object }
+
+  def interfaces
+    interfaces = Fixtures[:interfaces]
+    return interfaces if interfaces
+    file = get_fixture('interfaces.json')
+    Fixtures[:interfaces] = JSON.load(File.read(file))
   end
 
-  # Stub the Api method class to obtain all interfaces.
   before :each do
-    allow_message_expectations_on_nil
-    allow(described_class).to receive(:eapi)
-    allow(described_class.eapi).to receive(:Interface)
-    allow(described_class.eapi.Interface).to receive(:getall)
-      .and_return(all_interfaces)
+    allow(described_class.node).to receive(:api).with('interfaces')
+      .and_return(api)
+
+    allow(provider.node).to receive(:api).with('interfaces').and_return(api)
   end
 
   context 'class methods' do
+
+    before { allow(api).to receive(:getall).and_return(interfaces) }
 
     describe '.instances' do
       subject { described_class.instances }
 
       it { is_expected.to be_an Array }
 
-      it 'has two entries' do
-        expect(subject.size).to eq(2)
+      it 'has one entry' do
+        expect(subject.size).to eq(1)
       end
 
-      %w(Ethernet1 Management1).each do |name|
-        it "has an instance for interface #{name}" do
-          instance = subject.find { |p| p.name == name }
-          expect(instance).to be_a described_class
-        end
+      it "has an instance for interface Ethernet1" do
+        instance = subject.find { |p| p.name == 'Ethernet1' }
+        expect(instance).to be_a described_class
       end
 
       context 'eos_interface { Ethernet1: }' do
@@ -87,26 +85,10 @@ describe Puppet::Type.type(:eos_interface).provider(:eos) do
 
         include_examples 'provider resource methods',
                          name: 'Ethernet1',
-                         description: '',
-                         enable: :true,
-                         flowcontrol_receive: :off,
-                         flowcontrol_send: :on
+                         description: 'test interface',
+                         enable: :true
       end
 
-      context 'eos_interface { Management1: }' do
-        subject do
-          described_class.instances.find do
-            |p| p.name == 'Management1'
-          end
-        end
-
-        include_examples 'provider resource methods',
-                         name: 'Management1',
-                         description: '',
-                         enable: :true,
-                         flowcontrol_receive: :desired,
-                         flowcontrol_send: :desired
-      end
     end
 
     describe '.prefetch' do
@@ -115,9 +97,7 @@ describe Puppet::Type.type(:eos_interface).provider(:eos) do
           'Ethernet1' => Puppet::Type.type(:eos_interface)
             .new(name: 'Ethernet1'),
           'Ethernet2' => Puppet::Type.type(:eos_interface)
-            .new(name: 'Ethernet2'),
-          'Management1' => Puppet::Type.type(:eos_interface)
-            .new(name: 'Management1')
+            .new(name: 'Ethernet2')
         }
       end
       subject { described_class.prefetch(resources) }
@@ -131,158 +111,66 @@ describe Puppet::Type.type(:eos_interface).provider(:eos) do
 
       it 'sets the provider instance of the managed resource' do
         subject
-        %w(Ethernet1 Management1).each do |intf|
-          expect(resources[intf].provider.name).to eq(intf)
-        end
+        expect(resources['Ethernet1'].provider.description).to eq('test interface')
+        expect(resources['Ethernet1'].provider.enable).to eq :true
+      end
+
+      it 'does not set the provider instance of the unmanaged resource' do
+        subject
+        expect(resources['Ethernet2'].provider.description).to eq :absent
+        expect(resources['Ethernet2'].provider.enable).to eq :absent
       end
     end
   end
 
   context 'resource (instance) methods' do
 
-    let(:name) { provider.resource[:name] }
-    let(:eapi) { double }
-
-    before :each do
-      allow(provider).to receive(:eapi)
-      allow(provider.eapi).to receive(:Interface).and_return(eapi)
-    end
-
     describe '#create' do
-      before :each do
-        allow(eapi).to receive(:create).with(name).and_return(true)
-        allow(eapi).to receive(:set_shutdown).and_return(true)
-        allow(eapi).to receive(:set_description).and_return(true)
-        allow(eapi).to receive(:set_flowcontrol).and_return(true)
-      end
+      let(:name) { 'Ethernet1' }
 
-      it 'calls Interface#create(name) with the resource name' do
-        expect(eapi).to receive(:create).with(name)
+      it 'sets ensure to :present' do
+        expect(api).to receive(:create).with(resource[:name])
         provider.create
       end
 
-      it 'sets enable to the resource value' do
+      it 'sets enable on the resource' do
+        expect(api).to receive(:set_shutdown).with(name, value: false)
         provider.create
-        expect(provider.enable).to eq(provider.resource[:enable])
+        expect(provider.enable).to be_truthy
       end
 
-      it 'sets description to the resource value' do
+      it 'sets description on the resource' do
+        expect(api).to receive(:set_description).with(name, value: 'test interface')
         provider.create
-        expect(provider.description).to eq(provider.resource[:description])
-      end
-
-      it 'sets flowcontrol_send to the resource value' do
-        provider.create
-        value = provider.resource[:flowcontrol_send]
-        expect(provider.flowcontrol_send).to eq(value)
-      end
-
-      it 'sets flowcontrol_receive to the resource value' do
-        provider.create
-        value = provider.resource[:flowcontrol_receive]
-        expect(provider.flowcontrol_receive).to eq(value)
+        expect(provider.description).to eq('test interface')
       end
     end
 
     describe '#destroy' do
-      before :each do
-        allow(eapi).to receive(:create).with(name)
-        allow(eapi).to receive(:set_shutdown).and_return(true)
-        allow(eapi).to receive(:set_description).and_return(true)
-        allow(eapi).to receive(:set_flowcontrol).and_return(true)
-        allow(eapi).to receive(:delete).with(name).and_return(true)
-      end
-
-      it 'calls Interface#delete(name)' do
-        expect(eapi).to receive(:delete).with(name)
+      it 'sets ensure to :absent' do
+        expect(api).to receive(:delete).with(resource[:name])
         provider.destroy
-      end
-
-      context 'when the resource has been created' do
-        subject do
-          provider.create
-          provider.destroy
-        end
-
-        it 'clears the property hash' do
-          subject
-          expect(provider.instance_variable_get(:@property_hash))
-            .to eq(name: name, ensure: :absent)
-        end
       end
     end
 
     describe '#description=(value)' do
-      before :each do
-        allow(eapi).to receive(:set_description)
-          .with(name, value: 'foo')
-      end
-
-      it "calls Interface#set_description(#{name}, 'foo')" do
-        expect(eapi).to receive(:set_description)
-          .with(name, value: 'foo')
-        provider.description = 'foo'
-      end
-
       it 'updates description in the provider' do
-        expect(provider.description).not_to eq('foo')
+        expect(api).to receive(:set_description)
+          .with(resource[:name], value: 'foo')
         provider.description = 'foo'
         expect(provider.description).to eq('foo')
       end
     end
 
     describe '#enable=(value)' do
-      before :each do
-        allow(eapi).to receive(:set_shutdown)
-          .with(name, value: value)
-      end
-
-      %w(true, false).each do |val|
+     %w(true false).each do |val|
         let(:value) { !val }
-        it "calls Interface#set_shutdown(#{name}, #{val})" do
-          expect(eapi).to receive(:set_shutdown)
-            .with(name, value: !val)
+        let(:name) { 'Ethernet1' }
+
+        it "updates enable in the provider" do
+          expect(api).to receive(:set_shutdown).with(name, value: !val)
           provider.enable = val
-        end
-      end
-    end
-
-    %w(:on :off :desired).each do |val|
-      describe '#flowcontrol_send=(value)' do
-        before :each do
-          allow(eapi).to receive(:set_flowcontrol)
-            .with(name, 'send', value: val)
-        end
-
-        it "calls Interface#set_flowcontrol(#{name}, 'send', #{val})" do
-          expect(eapi).to receive(:set_flowcontrol)
-            .with(name, 'send', value: val)
-          provider.flowcontrol_send = val
-        end
-
-        it 'updates flowcontrol_receive in the provider' do
-          expect(provider.flowcontrol_receive).not_to eq(val)
-          provider.flowcontrol_send = val
-          expect(provider.flowcontrol_send).to eq(val)
-        end
-      end
-
-      describe '#flowcontrol_receive=(value)' do
-        before :each do
-          allow(eapi).to receive(:set_flowcontrol)
-            .with(name, 'receive', value: val)
-        end
-
-        it "calls Interface#set_flowcontrol(#{name}, 'receive', #{val})" do
-          expect(eapi).to receive(:set_flowcontrol)
-            .with(name, 'receive', value: val)
-          provider.flowcontrol_receive = val
-        end
-
-        it 'updates flowcontrol_receive in the provider' do
-          expect(provider.flowcontrol_receive).not_to eq(val)
-          provider.flowcontrol_receive = val
-          expect(provider.flowcontrol_receive).to eq(val)
+          expect(provider.enable).to eq(val)
         end
       end
     end
