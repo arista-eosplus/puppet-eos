2f1ebf473caf922f8785053f0d5df3ca96135e28
Peter Sprygada <sprygada@arista.com>
2015-03-12 07:57:37 -0400

    adds type and provider support for vxlan vlan to vni mapping
    This commit adds a new type eos_vxlan_vlan that can be used to manage
    the vtep vlan to vni mapping.  Below is an example use:
    
    eos_vxlan_vlan { '100':
        vni => '100'
    }
    
    The type is ensurable so vlan mappings can be created or destroyed.

---
+++ lib/puppet/provider/eos_vxlan_vlan/default.rb
@@ -0,0 +1,89 @@
+#
+# Copyright (c) 2014, Arista Networks, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#   Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+#   Neither the name of Arista Networks nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ARISTA NETWORKS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+require 'puppet/type'
+require 'pathname'
+
+module_lib = Pathname.new(__FILE__).parent.parent.parent.parent
+require File.join module_lib, 'puppet_x/eos/provider'
+
+Puppet::Type.type(:eos_vxlan_vlan).provide(:eos) do
+
+  # Create methods that set the @property_hash for the #flush method
+  mk_resource_methods
+
+  # Mix in the api as instance methods
+  include PuppetX::Eos::EapiProviderMixin
+
+  # Mix in the api as class methods
+  extend PuppetX::Eos::EapiProviderMixin
+
+  def self.instances
+    resources = node.api('interfaces').get('Vxlan1')
+    resources[:vlans].map do |(vlan, attrs)|
+      provider_hash = { name: vlan, ensure: :present, vni: attrs[:vni] }
+      new(provider_hash)
+    end
+  end
+
+  def initialize(resource = {})
+    super(resource)
+    @property_flush = {}
+  end
+
+  def vni=(val)
+    @property_flush[:vni] = val
+  end
+
+  def exists?
+    @property_hash[:ensure] == :present
+  end
+
+  def create
+    @property_flush = resource.to_hash
+  end
+
+  def destroy
+    @property_flush = resource.to_hash
+  end
+
+  def flush
+    api = node.api('interfaces')
+    desired_state = @property_hash.merge!(@property_flush)
+    case desired_state[:ensure]
+    when :present
+      api.update_vlan(desired_state[:name], desired_state[:vni])
+    when :absent
+      api.remove_vlan(desired_state[:name])
+    end
+    @property_hash = desired_state
+  end
+end
---
+++ spec/unit/puppet/provider/eos_vxlan_vlan/default_spec.rb
@@ -0,0 +1,152 @@
+#
+# Copyright (c) 2014, Arista Networks, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#   Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+#   Neither the name of Arista Networks nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ARISTA NETWORKS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+require 'spec_helper'
+
+include FixtureHelpers
+
+describe Puppet::Type.type(:eos_vxlan_vlan).provider(:eos) do
+
+  # Puppet RAL memoized methods
+  let(:resource) do
+    resource_hash = {
+      name: '100',
+      vni: '100',
+      provider: described_class.name
+    }
+    Puppet::Type.type(:eos_vxlan_vlan).new(resource_hash)
+  end
+
+  let(:provider) { resource.provider }
+
+  let(:api) { double('interfaces') }
+
+  def vxlan
+    vxlan = Fixtures[:vxlan]
+    return vxlan if vxlan
+    fixture('vxlan', dir: File.dirname(__FILE__))
+  end
+
+  before :each do
+    allow(described_class.node).to receive(:api).with('interfaces')
+      .and_return(api)
+
+    allow(provider.node).to receive(:api).with('interfaces').and_return(api)
+  end
+
+  context 'class methods' do
+
+    before { allow(api).to receive(:get).and_return(vxlan) }
+
+    describe '.instances' do
+      subject { described_class.instances }
+
+      it { is_expected.to be_an Array }
+
+      it 'has one entry' do
+        expect(subject.size).to eq(1)
+      end
+
+      it "has an instance for vlan 10" do
+        instance = subject.find { |p| p.name == '10' }
+        expect(instance).to be_a described_class
+      end
+
+      context 'eos_vxlan_vlan { 10: }' do
+        subject { described_class.instances.find { |p| p.name == '10' } }
+
+        include_examples 'provider resource methods',
+                         name: '10',
+                         vni: '10',
+                         ensure: :present
+      end
+
+    end
+
+    describe '.prefetch' do
+      let :resources do
+        {
+          '10' => Puppet::Type.type(:eos_vxlan_vlan).new(name: '10'),
+          '20' => Puppet::Type.type(:eos_vxlan_vlan).new(name: '20')
+        }
+      end
+      subject { described_class.prefetch(resources) }
+
+      it 'resource providers are absent prior to calling .prefetch' do
+        resources.values.each do |rsrc|
+          expect(rsrc.provider.vni).to eq(:absent)
+        end
+      end
+
+      it 'sets the provider instance of the managed resource' do
+        subject
+        expect(resources['10'].provider.vni).to eq('10')
+      end
+
+      it 'does not set the provider instance of the unmanaged resource' do
+        subject
+        expect(resources['20'].provider.vni).to eq :absent
+      end
+    end
+  end
+
+  context 'resource (instance) methods' do
+
+    describe '#create' do
+      before do
+        expect(api).to receive(:update_vlan)
+          .with(resource[:name], resource[:vni])
+      end
+
+      it 'sets ensure to :present' do
+        provider.create
+        provider.flush
+        expect(provider.ensure).to eq(:present)
+      end
+
+      it 'sets vni on the resource' do
+        provider.create
+        provider.flush
+        expect(provider.vni).to eq('100')
+      end
+    end
+
+    describe '#destroy' do
+      it 'sets ensure to :absent' do
+        resource[:ensure] = :absent
+        expect(api).to receive(:remove_vlan).with(resource[:name])
+        provider.destroy
+        provider.flush
+        expect(provider.ensure).to eq(:absent)
+      end
+    end
+  end
+end
