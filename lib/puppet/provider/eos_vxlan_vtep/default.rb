c63c2143bb43d0f7090e2a10c264d1a0c5818b0f
John Corbin <jcorbin@arista.com>
2015-04-29 16:13:23 -0700

    adds type and provider support for vxlan vtep flood addresses
    This commit adds a new type eos_vxlan_vtep that can be used to manage
    the global Vxlan VTEP flood list. Below is an example use:
    
    eos_vxlan_vtep { '1.1.1.1':
       interface => Vxlan1
       ensure => present
    }
    
    eos_vxlan_vtep { '2.2.2.2':
       interface => Vxlan1
       ensure => present
    }
    
    The type is ensurable so vtep flood addresses can be added or removed.

---
+++ lib/puppet/provider/eos_vxlan_vtep/default.rb
@@ -0,0 +1,70 @@
+#
+# Copyright (c) 2015, Arista Networks, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#   Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+#   Neither the name of Arista Networks nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ARISTA NETWORKS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+require 'puppet/type'
+require 'pathname'
+
+module_lib = Pathname.new(__FILE__).parent.parent.parent.parent
+require File.join module_lib, 'puppet_x/eos/provider'
+
+Puppet::Type.type(:eos_vxlan_vtep).provide(:eos) do
+  # Create methods that set the @property_hash for the #flush method
+  mk_resource_methods
+
+  # Mix in the api as instance methods
+  include PuppetX::Eos::EapiProviderMixin
+
+  # Mix in the api as class methods
+  extend PuppetX::Eos::EapiProviderMixin
+
+  def self.instances
+    resources = node.api('interfaces').get('Vxlan1')
+    return [] unless resources
+    resources[:flood_list].map do |name|
+      provider_hash = { name: name, ensure: :present }
+      new(provider_hash)
+    end
+  end
+
+  def exists?
+    @property_hash[:ensure] == :present
+  end
+
+  def create
+    node.api('interfaces').add_vtep('Vxlan1', resource[:name])
+    @property_hash = { name: resource[:name], ensure: :present }
+  end
+
+  def destroy
+    node.api('interfaces').remove_vtep('Vxlan1', resource[:name])
+    @property_hash = { name: resource[:name], ensure: :absent }
+  end
+end
