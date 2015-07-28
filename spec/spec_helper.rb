553797963cd7b3232870cb9abb051cac40aef31e
John Corbin <jcorbin@arista.com>
2015-07-27 20:17:22 -0700

    Rubocop cleanup.
    Still need to do a pass through all the spec tests to ensure
    compatability with upcoming rbeapi 0.3.0 release.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,11 @@
 require 'simplecov'
 require 'simplecov-json'
 require 'simplecov-rcov'
-SimpleCov.formatters =
-  [
-    SimpleCov::Formatter::HTMLFormatter,
-    SimpleCov::Formatter::JSONFormatter,
-    SimpleCov::Formatter::RcovFormatter
-  ]
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::JSONFormatter,
+  SimpleCov::Formatter::RcovFormatter
+]
 SimpleCov.start do
   add_filter '/spec/'
   add_filter '/.bundle/'
