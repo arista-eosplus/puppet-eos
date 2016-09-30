4e7484d70062523d6cb62b478c113907914317a5
Florin Vinti <f.vinti@gmail.com>
2016-09-30 16:08:59 +0200

    add prefixlist type and provider + spec tests

--- spec/support/shared_examples_for_providers.rb
+++ spec/support/shared_examples_for_providers.rb
@@ -5,3 +5,12 @@ RSpec.shared_examples 'provider resource methods' do |opts = {}|
     end
   end
 end
+
+RSpec.shared_examples 'provider resource properties' do |opts = {}|
+  namevar = opts[:name]
+  opts.each_pair do |property, value|
+    it "#{property} is #{value}" do
+      expect(rules[namevar].provider.get(property)).to eq(value)
+    end
+  end
+end
