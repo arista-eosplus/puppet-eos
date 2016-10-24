7a9eb9d7205320f0e240b2184d1cbcd64af354f0
Jere Julian <jere@arista.com>
2016-10-24 14:46:19 -0400

    Set wget progress style and puppet executable path

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,12 @@ PUPPET_URL="http://pm.puppetlabs.com/puppet-agent/2016.2.0/1.5.2/repos/eos/4/PC1
 RBEAPI='rbeapi-puppet-aio-1.0-1.swix'
 RBEAPI_URL="https://github.com/arista-eosplus/rbeapi/releases/download/v1.0/${RBEAPI}"
 
+WGET_OPTS="--progress=dot:binary"
+
 # Ensure the puppet agent is installed
 puppet_exists=`ls -l /mnt/flash/.extensions/${PUPPET}`
 if [ $? -ne 0 ]; then
-  wget -O /mnt/flash/${PUPPET} ${PUPPET_URL}
+  wget ${WGET_OPTS} -O /mnt/flash/${PUPPET} ${PUPPET_URL}
   cmds="copy flash:${PUPPET} extension:
 extension ${PUPPET}"
   FastCli -p 15 -c "${cmds}"
@@ -16,11 +18,16 @@ fi
 # Ensure rbeapi is installed
 rbeapi_exists=`ls -l /mnt/flash/.extensions/${RBEAPI}`
 if [ $? -ne 0 ]; then
-  wget -O /mnt/flash/${RBEAPI} ${RBEAPI_URL}
+  wget ${WGET_OPTS} -O /mnt/flash/${RBEAPI} ${RBEAPI_URL}
   cmds="copy flash:${RBEAPI} extension:
 extension ${RBEAPI}"
   FastCli -p 15 -c "${cmds}"
 fi
+
+puppet="/opt/puppetlabs/bin/puppet"
+
+sudo ${puppet} config set user root
+sudo ${puppet} config set group root
 
 # Show the installed packages and ensure eAPI is enabled
 cmds="show extension
@@ -34,3 +41,7 @@ FastCli -p 15 -c "${cmds}"
 if [ -f /etc/redhat-release ]; then
   rm -f /etc/redhat-release
 fi
+
+${puppet} describe --modulepath=/tmp/kitchen/data eos_switchconfig
+${puppet} resource --modulepath=/tmp/kitchen/data eos_switchconfig
+${puppet} apply --modulepath=/tmp/kitchen/data -e "eos_switchconfig{'running-config': content => template('eos/tmp_config'), staging_file => 'puppet-config', }"
